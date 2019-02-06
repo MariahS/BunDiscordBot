775125a31835d96e93a0a1caaa8383b798b230f8
MariahS <smith.mariah@live.com>
2019-02-06 11:45:14 -0500

    Helper gives percentile code

--- BunBot/BunBun.Discord/HelperExtentions.cs
+++ BunBot/BunBun.Discord/HelperExtentions.cs
@@ -19,5 +19,49 @@ namespace BunBun.Discord
                 return x.encounterID.GetHashCode();
             }
         }
+
+        public string PercentileClassification(int percentile)
+        {
+            if (percentile <= 24)
+                return "Grey";
+
+            if (percentile <= 49 && percentile >= 25)
+                return "Green";
+
+            if (percentile <= 74 && percentile >= 50)
+                return "Blue";
+
+            if (percentile <= 94 && percentile >= 75)
+                return "Purple";
+
+            if (percentile <= 99 && percentile >= 95)
+                return "Orange";
+
+            if (percentile == 100)
+                return "Yellow";
+
+            return "No ranking";
+        }
+
+        public string IconCode(string colorCode)
+        {
+            switch (colorCode)
+            {
+                case "Grey":
+                    return "3_96x96";
+                case "Green":
+                    return "7_96x96";
+                case "Blue":
+                    return "6_96x96";
+                case "Purple":
+                    return "4_96x96";
+                case "Orange":
+                    return "1_96x96";
+                case "Yellow":
+                    return "5_96x96";
+            }
+
+            return "0_96x96";
+        }
     }
 }
